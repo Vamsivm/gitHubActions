58dac9f5bbc57132ce45733fe6dde76c6b4ed5b0
Vamsivm <krishnavamsi.vm@gmail.com>
2025-09-28 14:56:21 +0530

    Initial commit

---
+++ src/main/java/com/acn/io/acitons/AcitonsApplication.java
@@ -0,0 +1,13 @@
+package com.acn.io.acitons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcitonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcitonsApplication.class, args);
+	}
+
+}
